71c41297e3b45038ac0bbcbe5947a5ae6abf541a
Sunmonu Oluwole <sunmonuoluwole@gmail.com>
2019-08-15 22:31:56 +0100

    dere

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const app = require('./app')
+const port = process.env.port || 3000
 app.get('/',(req,res)=>{
  res.send('welcome')
 })
-app.listen(3000, ()=> {
-    console.log('server is connected');
+app.listen(port, ()=> {
+    console.log('server is connected',port);
     
 })
